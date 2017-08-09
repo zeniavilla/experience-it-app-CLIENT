6cf6c06373052245df542fc1c08f848dddf3c103
Zenia <zeniavilla@hotmail.com>
2017-08-09 14:58:39 -0700

    Add Provider to index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     document.getElementById('root')
 );
 
