9a37046800b264401752b69ed7453498d18350ad
Zenia <zeniavilla@hotmail.com>
2017-08-10 14:14:15 -0700

    Differentiate get experiences and create experience

--- src/reducers/experiencesReducer.js
+++ src/reducers/experiencesReducer.js
@@ -1,8 +1,9 @@
 export const experiencesReducer = (state = [], action) => {
     switch(action.type) {
-        case 'CREATE_EXPERIENCE':
+        case 'GET_EXPERIENCES':
             return action.experiences;
-        
+        case 'CREATE_EXPERIENCE':
+            return state.concat(action.experience);
         default:
             return state;
     }
