46e3ead302173ff88e1ce23d251917e1f613e2b1
Zenia <zeniavilla@hotmail.com>
2017-08-10 13:20:01 -0700

    Remove combineReducers import

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers'
@@ -9,4 +9,4 @@ export default createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(...middleware)
-)
+);
