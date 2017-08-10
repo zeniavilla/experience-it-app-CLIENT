4be83d6a986a6c0f3cdc4db2cdfb38169c41b36a
Zenia <zeniavilla@hotmail.com>
2017-08-10 13:18:29 -0700

    Rename experiencesReducer in camel case

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
 import { combineReducers } from 'redux';
-import { experiencesReducer } from './experiences_reducer';
+import { experiencesReducer } from './experiencesReducer';
 
 export default combineReducers({ experiences: experiencesReducer });
