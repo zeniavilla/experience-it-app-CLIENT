f08c95d6c7a36f7746d4ebbb65a2e49a7216b7c7
Zenia <zeniavilla@hotmail.com>
2017-08-10 12:58:58 -0700

    Move store code to store.js

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers'
+
+const middleware = [thunk];
+
+export default createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middleware)
+)
