cc2aede4b4b4fe40c9b36f0c010fedaa1786254c
Zenia <zeniavilla@hotmail.com>
2017-08-10 08:41:44 -0700

    Setup App.js to fetch experiences and include Experiences component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Experiences from './Experiences';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      experiences: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/experiences')
+      .then(response => response.json())
+      .then(experiences => this.setState({ experiences }))
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        App
+          <Experiences experiences={this.state.experiences} />  
       </div>
     );
   }
