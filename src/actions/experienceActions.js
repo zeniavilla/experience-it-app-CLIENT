63631b00f182f1275c0e084d10a08b87f4167ce7
Zenia <zeniavilla@hotmail.com>
2017-08-10 14:12:20 -0700

    Add createExperience action

--- src/actions/experienceActions.js
+++ src/actions/experienceActions.js
@@ -3,8 +3,15 @@ const API_URL = process.env.REACT_APP_API_URL;
 // ** Action Creators **
 const setExperiences = experiences => {
     return { 
-        type: 'CREATE_EXPERIENCE',
+        type: 'GET_EXPERIENCES',
         experiences
+    }
+}
+
+const addExperience = experience => {
+    return {
+        type: 'CREATE_EXPERIENCE',
+        experience
     }
 }
 
@@ -15,5 +22,20 @@ export const getExperiences = () => {
             .then(response => response.json())
             .then(experiences => dispatch(setExperiences(experiences)))
             .catch(error => console.log(error));
+    }
+}
+
+export const createExperience = experience => {
+    return dispatch => {
+        return fetch(`${API_URL}/experiences`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ experience: experience })
+        })
+            .then(response => response.json())
+            .then(experience => dispatch(addExperience(experience)) )
+            .catch(error => console.log(error))
     }
 }
