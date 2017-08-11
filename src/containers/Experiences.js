14c3af43bad4b9153f8ac504ffbec5990869135b
Zenia <zeniavilla@hotmail.com>
2017-08-11 13:01:51 -0700

    Remove ExperienceForm component from Experiences

--- src/containers/Experiences.js
+++ src/containers/Experiences.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 
 import Experience from '../components/Experience';
 import './Experience.css';
-import ExperienceForm from '../containers/ExperienceForm';
 import { getExperiences } from '../actions/experienceActions';
 
 class Experiences extends Component {
@@ -16,7 +15,6 @@ class Experiences extends Component {
         return (
             <div>
                 <h3>Experiences Component</h3>
-                <ExperienceForm />
                 {this.props.experiences.map((experience, index) => <Experience key={index} experience={experience} />)}
             </div>
         )
