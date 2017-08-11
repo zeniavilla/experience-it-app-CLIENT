98c29cc595b8eeab464996cd237948579d61be5f
Zenia <zeniavilla@hotmail.com>
2017-08-11 10:16:35 -0700

    Remove state from App and move into components folder

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Experiences from '../containers/Experiences';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Experiences />  
+      </div>
+    );
+  }
+}
+
+export default App;
