ebb9ee63eae257d2feed70de652c4ebe2a55f634
Zenia <zeniavilla@hotmail.com>
2017-08-12 14:29:11 -0700

    Import Router and add new and index experience routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Experiences from '../containers/Experiences';
+import ExperienceForm from '../containers/ExperienceForm';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Experiences />  
-      </div>
+      <Router>
+          <div className="App">
+            <Switch>
+              <Route path="/(experiences|)" component={Experiences} />
+              <Route path="/describe-your-experience" component={ExperienceForm} />
+            </Switch>
+          </div>
+      </Router>
     );
   }
 }
