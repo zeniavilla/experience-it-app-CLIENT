00946b2b48cf3add19246caf2dfb58a0e8e2ba1d
Zenia <zeniavilla@hotmail.com>
2017-08-12 14:29:38 -0700

    Use react-bootstrap Image and Grid components for Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Image, Grid } from 'react-bootstrap';
 
 const Experience = (props) =>
     <div className="experience-card" key={props.experience.id}>
-        <div className="experience-img-wrapper">
-            <img src={props.experience.img_url} alt={props.experience.name} />
-        </div>
+        <Grid className="experience-img-wrapper" fluid>    
+            <Image src={props.experience.img_url} alt={props.experience.name} /> 
+        </Grid>
         <h2>{props.experience.name}</h2>
         <h4>{props.experience.location}</h4>
         <h4>♥ 0 likes</h4>
