592b11e340b78b2dda9a750e00692627dbe27635
Zenia <zeniavilla@hotmail.com>
2017-08-12 12:54:17 -0700

    Install react-bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store.js';
