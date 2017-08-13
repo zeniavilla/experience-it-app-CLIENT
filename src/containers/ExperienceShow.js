7f0c9920089effd1ff10f1e86fd964e93a54d5e8
Zenia <zeniavilla@hotmail.com>
2017-08-13 13:31:59 -0700

    Add fields to ExperienceShow container

--- src/containers/ExperienceShow.js
+++ src/containers/ExperienceShow.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Grid, Col, Image } from 'react-bootstrap';
 
-const ExperienceShow = ({ experience }) => 
-    <div>
-        <img src="http://ghk.h-cdn.co/assets/16/09/980x490/landscape-1457107485-gettyimages-512366437.jpg" />
-        <h3>Work? {experience.name}</h3>
+import './ExperienceShow.css'
+
+
+const ExperienceShow = ({ experience }) => {
+
+const backgroundStyle ={
+    backgroundImage: `url(${experience.img_url})`
+};
+
+    return (
+    <div className="experience-show-main clearfix">
+        <Col xs={6}>
+            <h1>{experience.name}</h1>
+            <h4>{experience.location}</h4>
+
+            <div className="experience-show-text category">{experience.category} experience</div>
+
+            <div className="experience-show-text body-text">{experience.time} total</div>
+
+            <div className="experience-show-text body-text">
+                Recommended time:
+                <div>{experience.recommended_times}</div>
+            </div>
+            
+            
+
+            <div className="experience-show-text">♥ 0 likes</div>
+            
+        </Col>
+        
+        <Col xs={6}>
+            <Grid className="show-img" style={backgroundStyle} fluid>
+                {/* <Image src={experience.img_url} /> */}
+            </Grid> 
+        </Col>
+
     </div>
+    )
+}
 
 
 const mapStateToProps = (state, ownProps) => {
