8f05c96a68df989559ff3a0a8611ef7575811e61
Zenia <zeniavilla@hotmail.com>
2017-08-13 11:15:35 -0700

    Make NavBar into own component and simplify App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 import Experiences from '../containers/Experiences';
 import ExperienceForm from '../containers/ExperienceForm';
 import ExperienceShow from '../containers/ExperienceShow';
+import NavBar from './NavBar';
 
 class App extends Component {
   render() {
     return (
       <Router>
           <div>
-            {/* Brand navbar  */}
-            <Navbar className="brand-navbar">
-              <Navbar.Header>
-              <Navbar.Brand>
-                <NavLink to="/" exact>Experience It</NavLink>
-              </Navbar.Brand>
-              </Navbar.Header>
-              <Nav pullRight>
-                <NavItem><NavLink to="/" exact>Experiences</NavLink></NavItem>
-                <NavItem><NavLink to="/describe-your-experience" exact>Add Experience</NavLink></NavItem>
-              </Nav>
-            </Navbar>
+              <NavBar />
 
-            <Navbar>
-              <Nav>
-                <NavDropdown eventKey={1} title="Categories" id="categories-dropdown">
-                  <MenuItem></MenuItem>
-                </NavDropdown>
-              </Nav>
-            </Navbar>
-
-            <Switch>
               <Route path="/(experiences|)" component={Experiences} />
               <Route path="/describe-your-experience" component={ExperienceForm} />
               <Route path="/experiences/:id" component={ExperienceShow} />
-            </Switch>
           </div>
       </Router>
     );
---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
+const NavBar = () => {
+  return (
+    <div>
+        <Navbar className="brand-navbar">
+            <Navbar.Header>
+            <Navbar.Brand>
+              <NavLink to="/" exact>Experience It</NavLink>
+            </Navbar.Brand>
+            </Navbar.Header>
+            <Nav pullRight>
+              <NavItem><NavLink to="/" exact>Experiences</NavLink></NavItem>
+              <NavItem><NavLink to="/describe-your-experience" exact>Add Experience</NavLink></NavItem>
+            </Nav>
+        </Navbar>
+
+        <Navbar>
+            <Nav>
+                <NavDropdown eventKey={1} title="Categories" id="categories-dropdown">
+                    <MenuItem></MenuItem>
+                </NavDropdown>
+            </Nav>
+        </Navbar>
+    </div>
+  )
+}
+
+            
+
+export default NavBar;
