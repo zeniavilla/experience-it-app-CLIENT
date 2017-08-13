e76cf4d4bc0bb311b9cb2eef96c6943e4d86313c
Zenia <zeniavilla@hotmail.com>
2017-08-12 17:37:50 -0700

    Change NavItems to NavLinks in App navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 import './App.css';
 import Experiences from '../containers/Experiences';
 import ExperienceForm from '../containers/ExperienceForm';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import ExperienceShow from '../containers/ExperienceShow';
 
 class App extends Component {
   render() {
     return (
       <Router>
           <div>
-            <Navbar>
+            {/* Brand navbar  */}
+            <Navbar className="brand-navbar">
               <Navbar.Header>
               <Navbar.Brand>
-                <a href="#">Experience It</a>
+                <NavLink to="/" exact>Experience It</NavLink>
               </Navbar.Brand>
               </Navbar.Header>
               <Nav pullRight>
-                <NavItem eventKey={1} href="/experiences">Experiences</NavItem>
-                <NavItem eventKey={2} href="/describe-your-experience">Add Experience</NavItem>
+                <NavItem><NavLink to="/" exact>Experiences</NavLink></NavItem>
+                <NavItem><NavLink to="/describe-your-experience" exact>Add Experience</NavLink></NavItem>
+              </Nav>
+            </Navbar>
+
+            <Navbar>
+              <Nav>
+                <NavDropdown eventKey={1} title="Categories" id="categories-dropdown">
+                  <MenuItem></MenuItem>
+                </NavDropdown>
               </Nav>
             </Navbar>
 
             <Switch>
               <Route path="/(experiences|)" component={Experiences} />
               <Route path="/describe-your-experience" component={ExperienceForm} />
+              <Route path="/experiences/:id" component={ExperienceShow} />
             </Switch>
           </div>
       </Router>
