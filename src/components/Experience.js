7fa1b0b5b0199dfde9918766dd226b2d88060403
Zenia <zeniavilla@hotmail.com>
2017-08-13 10:41:48 -0700

    add NavLink to experienceShow page for each Experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Image, Grid } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
-const Experience = (props) =>
-    <div className="experience-card" key={props.experience.id}>
-        <Grid className="experience-img-wrapper" fluid>    
-            <Image src={props.experience.img_url} alt={props.experience.name} /> 
-        </Grid>
-        <h2>{props.experience.name}</h2>
-        <h4>{props.experience.location}</h4>
-        <h4>♥ 0 likes</h4>
-    </div>
+const Experience = props =>
+<div>
+    <NavLink to={`/experiences/${props.experience.id}`}>
+        <div className="experience-card" key={props.experience.id}>
+            <Grid className="experience-img-wrapper" fluid>    
+                <Image src={props.experience.img_url} alt={props.experience.name} /> 
+            </Grid>
+            <h2>{props.experience.name}</h2>
+            <h4>{props.experience.location}</h4>
+            <h4>♥ 0 likes</h4>
+        </div>
+    </NavLink>
+</div>
 
 export default Experience;
