30a126c0f25e587b01fa593cb69cccef606f19ad
Zenia <zeniavilla@hotmail.com>
2017-08-14 13:03:12 -0700

    Add currentExperienceReducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
 import { experiencesReducer } from './experiencesReducer';
 import { experienceFormData } from './experienceFormData';
+import { currentExperienceReducer } from './currentExperienceReducer';
 
-export default combineReducers({ experiences: experiencesReducer, experienceFormData: experienceFormData });
+export default combineReducers({ 
+    experiences: experiencesReducer, 
+    experienceFormData: experienceFormData, 
+    currentExperience: currentExperienceReducer 
+});
