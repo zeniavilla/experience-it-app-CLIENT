2af3c7e1e7311bca24380810140492b8264f60db
Zenia <zeniavilla@hotmail.com>
2017-08-14 13:12:53 -0700

    Remove Experience.css import for ExperienceNew container

--- src/containers/ExperienceNew.js
+++ src/containers/ExperienceNew.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import './ExperienceForm.css'
 import ExperienceForm from '../components/ExperienceForm';
 
 import { updateExperienceFormData } from '../actions/experienceFormActions';
