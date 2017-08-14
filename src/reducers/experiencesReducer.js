1a329bd87ee73a948081bf314a37433cba9e5fbd
Zenia <zeniavilla@hotmail.com>
2017-08-14 13:01:28 -0700

    Add whitespace for readability in experiencesReducer

--- src/reducers/experiencesReducer.js
+++ src/reducers/experiencesReducer.js
@@ -1,10 +1,14 @@
 export const experiencesReducer = (state = [], action) => {
     switch(action.type) {
+
         case 'GET_EXPERIENCES':
             return action.experiences;
+
         case 'CREATE_EXPERIENCE':
             return state.concat(action.experience);
+
         default:
             return state;
+            
     }
 }
