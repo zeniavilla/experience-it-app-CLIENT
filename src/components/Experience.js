aa3ae6b60cf6d6b7f1af8ab40d7eaf5966252127
Zenia <zeniavilla@hotmail.com>
2017-08-15 09:20:50 -0700

    Refactor variables and view

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,18 +2,34 @@ import React from 'react';
 import { Image, Grid } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
-const Experience = props =>
-<div>
-    <NavLink to={`/experiences/${props.experience.id}`}>
-        <div className="experience-card" key={props.experience.id}>
-            <Grid className="experience-img-wrapper" fluid>    
-                <Image src={props.experience.img_url} alt={props.experience.name} /> 
-            </Grid>
-            <h2>{props.experience.name}</h2>
-            <h4>{props.experience.location}</h4>
-            <h4>♥ 0 likes</h4>
+import '../font-awesome-4.7.0/css/font-awesome.css'
+
+const Experience = props => {
+
+    const { id, name, location, img_url, likes, liked } = props.experience;
+    const handleOnClick = props.handleOnClick;
+
+    return (
+        <div>
+            <NavLink to={`/experiences/${id}`}>
+                <div className="experience-card" key={id}>
+                    <Grid className="experience-img-wrapper" fluid>    
+                        <Image src={img_url} alt={name} /> 
+                    </Grid>
+                    <h2>{name}</h2>
+                    <h4>{location}</h4>
+                    <h4><span className="change-icon" onClick={handleOnClick}>
+                        { (liked)
+                        ? <i id={id} className="fa fa-heart"></i>
+                        : <i id={id} className="fa fa-heart-o"></i>
+                        }
+                    </span>
+                    {likes} likes
+                    </h4>
+                </div>
+            </NavLink>
         </div>
-    </NavLink>
-</div>
+    )
+}
 
 export default Experience;
