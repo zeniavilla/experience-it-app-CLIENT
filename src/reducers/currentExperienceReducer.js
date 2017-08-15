00cf7ab8ae595cce0e7a55947c1181a953d92810
Zenia <zeniavilla@hotmail.com>
2017-08-15 09:22:12 -0700

    Add LIKE_EXPERIENCE case to currentExperiencesReducer

--- src/reducers/currentExperienceReducer.js
+++ src/reducers/currentExperienceReducer.js
@@ -4,6 +4,9 @@ export const currentExperienceReducer = (state = [], action) => {
         case 'GET_EXPERIENCE_ITEM':
             return action.experience;
 
+        case 'LIKE_EXPERIENCE':
+            return state.concat(action.experience);
+
         default:
             return state;
     }
