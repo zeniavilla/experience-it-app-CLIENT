5d16279abf43e58dac6ebac3cdd5163866783cc9
Zenia <zeniavilla@hotmail.com>
2017-08-16 19:05:31 -0700

    Add sort by id to experiences reducer

--- src/reducers/experiencesReducer.js
+++ src/reducers/experiencesReducer.js
@@ -1,8 +1,18 @@
+const compare = (a, b) => {
+    if (a.id < b.id) {
+        return -1;
+    } else if (a.id > b.id) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 export const experiencesReducer = (state = [], action) => {
     switch(action.type) {
 
         case 'GET_EXPERIENCES':
-            return action.experiences;
+            return (action.experiences).sort(compare);
 
         case 'CREATE_EXPERIENCE':
             return state.concat(action.experience);
