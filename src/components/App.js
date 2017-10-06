232eec3d84252f49bf42407662aebe56f9e238c8
Zenia <zeniavilla@hotmail.com>
2017-10-06 09:37:25 -0700

    Import Media.css into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
+import './Media.css';
 import Experiences from '../containers/Experiences';
 import ExperienceNew from '../containers/ExperienceNew';
 import NavBar from './NavBar';
